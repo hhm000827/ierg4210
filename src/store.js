3c757f55066ae56d0efbe652cbabad228627b6a1
hhm000827 <hauhoman@yahoo.com.hk>
2023-01-31 16:20:17 +0800

    add redux-persist to save redux status when reloading page

--- src/store.js
+++ src/store.js
@@ -1,8 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import shoppingCartReducer from "./components/shoppingCart/ShoppingCartSlice";
 
-export default configureStore({
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedShoppingCartReducer = persistReducer(persistConfig, shoppingCartReducer);
+
+const store = configureStore({
   reducer: {
-    shoppingCart: shoppingCartReducer,
+    shoppingCart: persistedShoppingCartReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+const persistor = persistStore(store);
+export { store, persistor };
