933305e7ca7277861a7c444d1eb2bff140be3881
hhm000827 <hauhoman@yahoo.com.hk>
2023-02-05 21:49:10 +0800

    add pagination in Home page

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,26 @@
 import axios from "axios";
 import lang from "lodash/lang";
 import { memo, useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 import { Card } from "../components/card/Card";
 
-const Home = () => {
+const DisplayedProduct = (props) => {
+  return (
+    <div className="grid h-auto gap-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+      {!lang.isEmpty(props.products) &&
+        props.products.map((product) => (
+          <div key={`home-${product.name}`}>
+            <Card product={product} />
+          </div>
+        ))}
+    </div>
+  );
+};
+
+function Home() {
   const [products, setProducts] = useState([]);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 6;
 
   useEffect(() => {
     axios
@@ -13,16 +29,33 @@ const Home = () => {
       .catch((e) => console.error(e));
   }, []);
 
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = products.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(products.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % products.length;
+    console.log(`User requested page number ${event.selected}, which is offset ${newOffset}`);
+    setItemOffset(newOffset);
+  };
+
   return (
-    <div className="grid h-auto gap-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {!lang.isEmpty(products) &&
-        products.map((product) => (
-          <div key={`home-${product.name}`}>
-            <Card product={product} />
-          </div>
-        ))}
-    </div>
+    <>
+      <DisplayedProduct products={currentItems} />
+      <ReactPaginate
+        className="btn-group gap-2 flex justify-center"
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+      />
+    </>
   );
-};
+}
 
 export default memo(Home);
